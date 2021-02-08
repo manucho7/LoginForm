3e396a993695484ccc8f9551873a05f9b2fd5860
manucho <manuel.blanco77@gmail.com>
2021-02-08 11:42:04 -0300

    login component initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
 import './App.css';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
         <h1>React Form</h1>
+        <Login />
     </div>
   );
 }
